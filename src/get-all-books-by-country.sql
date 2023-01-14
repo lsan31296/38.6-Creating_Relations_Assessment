8b4eaaad1624a4e3918779c13141088e1e37944d
Luis E. Sanchez Jr <113560203+lsan31296@users.noreply.github.com>
2023-01-13 22:13:12 -0500

    Added a query to list all author and book fields for books written by an author from China or Turkey.

---
+++ src/get-all-books-by-country.sql
@@ -0,0 +1,2 @@
+--Write a query to return a list of all book and author-related fields for all books written by an author from "China" or "Turkey".
+SELECT * FROM books b JOIN authors a ON b.author_id = a.author_id WHERE a.nationality = 'China' OR a.nationality = 'Turkey';
