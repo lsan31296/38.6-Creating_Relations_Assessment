151c4b4f604a567d07fc3f6f7d621b8478a59327
Luis E. Sanchez Jr <113560203+lsan31296@users.noreply.github.com>
2023-01-13 21:58:25 -0500

    Added a query to portray list of all books and author fields for books written by Amy Tan.

---
+++ src/get-all-books-by-author.sql
@@ -0,0 +1,2 @@
+--Write a query to return a list of all book and author-related fields for all books written by "Amy Tan".
+SELECT * FROM books b JOIN authors a ON a.author_id = b.author_id WHERE a.author_name = 'Amy Tan';
