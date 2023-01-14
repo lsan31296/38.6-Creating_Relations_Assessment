2cd5629532292713a95b6b95f1b9b49c9484abb6
Luis E. Sanchez Jr <113560203+lsan31296@users.noreply.github.com>
2023-01-13 22:38:38 -0500

    Added query that lists all author/books fields for books that have a title length less than 25 characters.

---
+++ src/get-all-authors-by-title-length.sql
@@ -0,0 +1,2 @@
+--Write a query to return a list of all author and book-related fields for books whose titles are longer than 25 characters.
+SELECT * FROM books b JOIN authors a ON b.author_id = a.author_id WHERE LENGTH(b.title) > 25;
