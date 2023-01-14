66d9519eb614950b5d07a8b679daadc6dacd21be
Luis E. Sanchez Jr <113560203+lsan31296@users.noreply.github.com>
2023-01-13 22:31:51 -0500

    Added a query that lists all book/author fields for books written before 2005 by non-US authors.

---
+++ src/get-all-books-before-year-by-nationality.sql
@@ -0,0 +1,2 @@
+--Write a query to return a list of all book and author-related fields for all books written before 2005 by non-US authors.
+SELECT * FROM books b JOIN authors a ON b.author_id = a.author_id WHERE b.publication_year < 2005 AND a.nationality <> 'United States of America';
